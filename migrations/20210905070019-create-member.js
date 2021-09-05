447250f0ada7b0e4a07faea0d0be78aa99fc46e3
hyunlee214 <hyun110214@naver.com>
2021-09-05 16:30:47 +0900

    모델 정의중..

--- migrations/20210905070019-create-member.js
+++ migrations/20210905070019-create-member.js
@@ -1,7 +1,8 @@
 'use strict';
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('members', {
+    return await queryInterface.createTable('members', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,10 +10,12 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
@@ -25,6 +28,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('members');
+    return await queryInterface.dropTable('members');
   }
 };
