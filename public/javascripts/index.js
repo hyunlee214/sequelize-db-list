9d48e0ea9b4d2779b6e48769985e19fd52436608
hyunlee214 <hyun110214@naver.com>
2021-09-07 09:59:11 +0900

    html - index.js 매핑

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,15 @@
 window.onload = () => {
   console.log('test onload');
+  
+  const memberForm = document.getElementById('member-form');
+  memberForm.addEventListener('submit', async (evt) => {
+
+    const username = document.getElementById('input-username').value;
+    const password = document.getElementById('input-password').value;
+    const obj = {
+      username: username,
+      password: password
+    };
+    await axios.post('/member', obj);
+    })
 };
